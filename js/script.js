78589beb1b265216fcc1c90182987053b35b18e1
Robert Fydrych <fydrych.robert.w@gmail.com>
2023-02-15 23:15:29 +0000

    new function added, class changed

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
     };
 
     const removeNameClick = () => {
-        const header = document.querySelector(".js-header");
+        const header = document.querySelector(".js-header-remove");
         const myName = document.querySelector(".js-name");
         header.remove();
         myName.remove();
@@ -15,49 +15,59 @@
         const themeName = document.querySelector(".js-themeName");
         body.classList.toggle("cold");
         themeName.innerText = body.classList.contains("cold") ? "warm" : "cool";
+        
     };
 
     const init = () => {
         const click1 = document.querySelector(".js-button");
         click1.addEventListener("click", removeNameClick);
         welcome();
+        button2.addEventListener("click", toggleBackground);
+        button2.addEventListener("click", onChangeBackgroundClick);
+    };
+
+    const button2 = document.querySelector(".js-button2");
+
+    const onChangeBackgroundClick = () => {
+        table__cellHeader.classList.toggle("cold1");
+        t2.classList.toggle("cold1");
+        t3.classList.toggle("cold1");
+        t4.classList.toggle("cold1");
+        t5.classList.toggle("cold1");
+        t6.classList.toggle("cold1");
+        t7.classList.toggle("cold1");
+        t8.classList.toggle("cold1");
+        t9.classList.toggle("cold1");
+        t10.classList.toggle("cold1");
+        t11.classList.toggle("cold1");
+        t12.classList.toggle("cold1");
+        headerTitle.classList.toggle("cold1");
+        jsHeader.classList.toggle("cold1");
+        jsHeader2.classList.toggle("cold1");
+        jsHeader3.classList.toggle("cold1");
+        jsHeader4.classList.toggle("cold1");
+        jsLink.classList.toggle("cold2");
+        jsLink2.classList.toggle("cold2");
+        jsLink3.classList.toggle("cold2");
+        jsLink4.classList.toggle("cold2");
+        button2.classList.toggle("cold3");
+
+        
     };
 
     init();
 
-    const button2 = document.querySelector(".js-button2");
-    button2.addEventListener("click", toggleBackground);
 
-    const header__title = document.querySelector(".header__title");
-    const js__header = document.querySelector(".js__header");
-    const js__header2 = document.querySelector(".js__header2");
-    const js__header3 = document.querySelector(".js__header3");
-    const js__header4 = document.querySelector(".js__header4");
+    const headerTitle = document.querySelector(".js-header-title");
+    const jsHeader = document.querySelector(".js-header");
+    const jsHeader2 = document.querySelector(".js-header2");
+    const jsHeader3 = document.querySelector(".js-header3");
+    const jsHeader4 = document.querySelector(".js-header4");
 
-    button2.addEventListener("click", () => {
-        header__title.classList.toggle("cold1");
-        js__header.classList.toggle("cold1");
-        js__header2.classList.toggle("cold1");
-        js__header3.classList.toggle("cold1");
-        js__header4.classList.toggle("cold1");
-    });
-
-    const js__link = document.querySelector(".js__link");
-    const js__link2 = document.querySelector(".js__link2");
-    const js__link3 = document.querySelector(".js__link3");
-    const js__link4 = document.querySelector(".js__link4");
-
-
-    button2.addEventListener("click", () => {
-        js__link.classList.toggle("cold2");
-        js__link2.classList.toggle("cold2");
-        js__link3.classList.toggle("cold2");
-        js__link4.classList.toggle("cold2");
-    });
-
-    button2.addEventListener("click", () => {
-        button2.classList.toggle("cold3");
-    });
+    const jsLink = document.querySelector(".js-link");
+    const jsLink2 = document.querySelector(".js-link2");
+    const jsLink3 = document.querySelector(".js-link3");
+    const jsLink4 = document.querySelector(".js-link4");
 
     const table__cellHeader = document.querySelector(".table__cellHeader")
     const t2 = document.querySelector(".t2")
@@ -71,21 +81,6 @@
     const t10 = document.querySelector(".t10")
     const t11 = document.querySelector(".t11")
     const t12 = document.querySelector(".t12")
-
-    button2.addEventListener("click", () => {
-        table__cellHeader.classList.toggle("cold1");
-        t2.classList.toggle("cold1");
-        t3.classList.toggle("cold1");
-        t4.classList.toggle("cold1");
-        t5.classList.toggle("cold1");
-        t6.classList.toggle("cold1");
-        t7.classList.toggle("cold1");
-        t8.classList.toggle("cold1");
-        t9.classList.toggle("cold1");
-        t10.classList.toggle("cold1");
-        t11.classList.toggle("cold1");
-        t12.classList.toggle("cold1");
-    });
 }
 
 
